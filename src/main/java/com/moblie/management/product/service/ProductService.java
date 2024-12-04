d3d346246320e095d3304639e9fe55afdc7a4a80
JS <zks145@naver.com>
2024-12-04 16:30:04 +0900

    feat: 상품 서비스 클래스 코드 추가

---
+++ src/main/java/com/moblie/management/product/service/ProductService.java
@@ -0,0 +1,18 @@
+package com.moblie.management.product.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+    // 상품 엑셀 파일 자동호출
+    public void createAutoProduct() {
+
+    }
+
+    // 상품 수동 입력
+
+    // 상품 정보 수정
+
+    // 상품 삭제
+}
