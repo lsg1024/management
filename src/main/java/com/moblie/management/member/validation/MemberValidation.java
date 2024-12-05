1737ab7316319005236660b6a96edfd971677307
lsg1024 <zks145@naver.com>
2024-12-05 15:52:00 +0900

    feat: 유저 검증 및 토큰 검증 validation 추가

--- src/main/java/com/moblie/management/member/validation/MemberValidation.java
+++ src/main/java/com/moblie/management/member/validation/MemberValidation.java
@@ -1,7 +1,31 @@
 package com.moblie.management.member.validation;
 
+import com.moblie.management.exception.CustomException;
+import com.moblie.management.exception.ErrorCode;
+import com.moblie.management.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public class MemberValidation {
 
+    private final MemberRepository memberRepository;
 
+    public static void checkConfirmPassword(String password, String confirm_password) {
+        if (!password.equals(confirm_password)) {
+            throw new CustomException(ErrorCode.REQUEST_FAILED, "비밀번호가 일치하지 않습니다.");
+        }
+    }
+
+    public static void checkEmail(String email) {
+        if (email == null || email.isBlank()) {
+            throw new CustomException(ErrorCode.REQUEST_FAILED, "옳바른 사용자 정보를 입력해주세요");
+        }
+    }
+
+    public static void checkPassword(String password) {
+        if (password == null || password.isBlank()) {
+            throw new CustomException(ErrorCode.REQUEST_FAILED, "옳바른 사용자 정보를 입력해주세요");
+        }
+    }
 
 }
