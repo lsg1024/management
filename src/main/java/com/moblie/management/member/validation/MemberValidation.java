4d7e4bace43eec275e3c5ed9e932ce8ea15c1ca0
JS <zks145@naver.com>
2024-12-08 20:50:19 +0900

    refactor: message 에러 반환 형식 통일

--- src/main/java/com/moblie/management/member/validation/MemberValidation.java
+++ src/main/java/com/moblie/management/member/validation/MemberValidation.java
@@ -12,19 +12,19 @@ public class MemberValidation {
 
     public static void checkConfirmPassword(String password, String confirm_password) {
         if (!password.equals(confirm_password)) {
-            throw new CustomException(ErrorCode.REQUEST_FAILED, "비밀번호가 일치하지 않습니다.");
+            throw new CustomException(ErrorCode.ERROR_400, "비밀번호가 일치하지 않습니다.");
         }
     }
 
     public static void checkEmail(String email) {
         if (email == null || email.isBlank()) {
-            throw new CustomException(ErrorCode.REQUEST_FAILED, "옳바른 사용자 정보를 입력해주세요");
+            throw new CustomException(ErrorCode.ERROR_400, "옳바른 사용자 정보를 입력해주세요");
         }
     }
 
     public static void checkPassword(String password) {
         if (password == null || password.isBlank()) {
-            throw new CustomException(ErrorCode.REQUEST_FAILED, "옳바른 사용자 정보를 입력해주세요");
+            throw new CustomException(ErrorCode.ERROR_400, "옳바른 사용자 정보를 입력해주세요");
         }
     }
 
--- src/main/java/com/moblie/management/redis/validation/TokenValidation.java
+++ src/main/java/com/moblie/management/redis/validation/TokenValidation.java
@@ -7,13 +7,13 @@ public class TokenValidation {
 
     public static void checkRefreshToken(String nickname) {
         if (nickname == null) {
-            throw new CustomException(ErrorCode.REQUEST_FAILED, "유효하지 않은 토큰 값입니다.");
+            throw new CustomException(ErrorCode.ERROR_400, "유효하지 않은 토큰 값입니다.");
         }
     }
 
     public static void checkRefreshTokenType(String category) {
         if (!category.equals("refresh")) {
-            throw new CustomException(ErrorCode.REQUEST_FAILED, "유효하지 않은 토큰 값입니다.");
+            throw new CustomException(ErrorCode.ERROR_400, "유효하지 않은 토큰 값입니다.");
         }
     }
 }
