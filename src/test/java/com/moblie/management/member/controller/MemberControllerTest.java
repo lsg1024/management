f6be79e539b908b0471a462bf537484a01227367
JS <zks145@naver.com>
2024-12-08 20:53:03 +0900

    style: dto 이름 변경에 따른 수정

--- src/test/java/com/moblie/management/member/controller/MemberControllerTest.java
+++ src/test/java/com/moblie/management/member/controller/MemberControllerTest.java
@@ -7,12 +7,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.doNothing;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -42,7 +40,7 @@ class MemberControllerTest {
         String requestBody = objectMapper.writeValueAsString(signDto);
 
         //when
-        doNothing().when(memberService).signUpMember(signDto);
+        doNothing().when(memberService).signUp(signDto);
 
         //then
         mockMvc.perform(post("/signup")
