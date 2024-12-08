38d5fda87073989dde60653f425a81df833066f6
JS <zks145@naver.com>
2024-12-08 20:45:54 +0900

    fix: import 선언 @ID 오류 수정

--- src/main/java/com/moblie/management/redis/domain/CertificationNumberToken.java
+++ src/main/java/com/moblie/management/redis/domain/CertificationNumberToken.java
@@ -1,7 +1,6 @@
 package com.moblie.management.redis.domain;
 
-import com.moblie.management.member.dto.MemberDto;
-import jakarta.persistence.Id;
+import org.springframework.data.annotation.Id;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.redis.core.RedisHash;
