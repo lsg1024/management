4feaa43799a142185c5840c8c53f1a98d0cd4315
JS <zks145@naver.com>
2024-12-10 23:11:29 +0900

    chore: CustomException 생성자 변수 변경

--- src/main/java/com/moblie/management/exception/CustomException.java
+++ src/main/java/com/moblie/management/exception/CustomException.java
@@ -2,20 +2,19 @@ package com.moblie.management.exception;
 
 import lombok.Getter;
 
+@Getter
 public class CustomException extends RuntimeException {
     private final ErrorCode errorCode;
     private final String errorMessage;
+
+    public CustomException(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+        this.errorMessage = errorCode.getMessage();
+    }
 
     public CustomException(ErrorCode errorCode, String errorMessage) {
         this.errorCode = errorCode;
         this.errorMessage = errorMessage;
     }
-
-    public ErrorCode getErrorCode() {
-        return errorCode;
-    }
-
-    public String getErrorMessage() {
-        return errorMessage;
-    }
 }
+
