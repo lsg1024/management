81b17e975d34977866133a7b379c5c41451e2447
JS <zks145@naver.com>
2024-12-15 14:48:28 +0900

    feat: 엑셀 처리 메서드 추가

--- src/main/java/com/moblie/management/product/excel/ExcelSheetUtils.java
+++ src/main/java/com/moblie/management/product/excel/ExcelSheetUtils.java
@@ -1,11 +1,10 @@
 package com.moblie.management.product.excel;
 
+import com.moblie.management.product.dto.ProductDto;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FilenameUtils;
-import org.apache.poi.openxml4j.exceptions.NotOfficeXmlFileException;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -14,30 +13,72 @@ import org.jsoup.select.Elements;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+@Slf4j
 public class ExcelSheetUtils {
 
     public static Workbook getSheets(MultipartFile file) throws IOException {
         String extension = FilenameUtils.getExtension(file.getOriginalFilename());
 
-        Workbook workbook = null;
+        Workbook workbook;
 
-        if (extension == null) {
-            throw new IllegalArgumentException("파일 확장자를 확인할 수 없습니다.");
+        if (extension == null || file.getSize() == 0) {
+            throw new IllegalArgumentException("파일 확장자가 없거나 파일이 비어 있습니다.");
         }
 
         try {
-            workbook = new XSSFWorkbook(file.getInputStream());
-        } catch (NotOfficeXmlFileException e) {
-            workbook = convertHtmlToWorkbook(file);
+            if ("xlsx".equalsIgnoreCase(extension)) {
+                // XLSX 파일 처리
+                workbook = new XSSFWorkbook(file.getInputStream());
+            } else if ("xls".equalsIgnoreCase(extension)) {
+                // XLS 파일 처리
+                workbook = new HSSFWorkbook(file.getInputStream());
+            } else {
+                throw new IllegalArgumentException("지원하지 않는 파일 형식입니다: " + extension);
+            }
         } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        if (workbook == null) {
-            throw new IllegalArgumentException("파일이 비어있습니다");
+            workbook = convertHtmlToWorkbook(file);
         }
         return workbook;
+    }
+
+    public static void formatProductExcelData(Sheet workSheet, ProductDto.productsInfo productsInfo, List<Integer> targetColum) {
+
+        productsInfo.products = new ArrayList<>();
+
+        for (int i = 1; i < workSheet.getPhysicalNumberOfRows(); i++) {
+            Row row = workSheet.getRow(i);
+
+            if (row == null) continue;
+
+            List<String> columnValues = targetColum.stream()
+                    .map(index -> extractedExcelCellData(row, index))
+                    .toList();
+
+            ProductDto.createProduct product = new ProductDto.createProduct(
+                    columnValues.get(0), // modelNumber
+                    columnValues.get(1), // factory
+                    columnValues.get(2), // modelClassification
+                    columnValues.get(3), // goldType
+                    columnValues.get(4), // goldColor
+                    columnValues.get(5), // modelWeight
+                    columnValues.get(6)  // modelComment
+            );
+
+            log.info("formatProductExcelData product {}", product);
+
+            productsInfo.products.add(product);
+
+        }
+
+        log.info("formatProductExcelData productInfo data {}", productsInfo.products.size());
+    }
+
+    private static String extractedExcelCellData(Row row, int target) {
+        Cell cell = row.getCell(target);
+        return cell != null ? cell.getStringCellValue() : null; 
     }
 
     // Html 데이터 테이블 읽어오기
