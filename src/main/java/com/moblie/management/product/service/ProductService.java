71d102a0b62d6fe60e5cc9dea83ce4399eb708e1
JS <zks145@naver.com>
2024-12-17 23:07:48 +0900

    refactor: 중복 코드 별도 메서드(createProductProcess)로 분리

--- src/main/java/com/moblie/management/product/service/ProductService.java
+++ src/main/java/com/moblie/management/product/service/ProductService.java
@@ -2,6 +2,8 @@ package com.moblie.management.product.service;
 
 import com.moblie.management.exception.CustomException;
 import com.moblie.management.exception.ErrorCode;
+import com.moblie.management.factory.domain.FactoryEntity;
+import com.moblie.management.factory.repository.FactoryRepository;
 import com.moblie.management.product.domain.ProductEntity;
 import com.moblie.management.product.dto.ProductDto;
 import com.moblie.management.product.repository.ProductRepository;
@@ -9,18 +11,16 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
-import static com.moblie.management.product.excel.ExcelSheetUtils.*;
+import static com.moblie.management.product.excel.ExcelProduct.formatProductExcelData;
+import static com.moblie.management.product.validation.ProductValidation.*;
+import static com.moblie.management.utils.ExcelSheetUtil.*;
 
 @Slf4j
 @Service
@@ -29,6 +29,7 @@ import static com.moblie.management.product.excel.ExcelSheetUtils.*;
 public class ProductService {
 
     private final ProductRepository productRepository;
+    private final FactoryRepository factoryRepository;
 
     //상품 엑셀 파일 자동호출
     @Transactional
@@ -41,71 +42,73 @@ public class ProductService {
 
         formatProductExcelData(worksheet, productsInfo, Arrays.asList(2, 3, 6, 7, 13, 8, 12));
 
-        List<ProductEntity> products = new ArrayList<>();
-        List<String> errors = new ArrayList<>();
-
-        extractedProductInfo(productsInfo, products, errors);
-
-        productRepository.saveAll(products);
-
-        return errors;
+        return createProductProcess(productsInfo);
     }
-
+    
     //상품 수동 입력
     @Transactional
     public List<String> createManualProduct(ProductDto.productsInfo productsInfo) {
 
-        List<ProductEntity> products = new ArrayList<>();
-        List<String> errors = new ArrayList<>();
-
-        extractedProductInfo(productsInfo, products, errors);
-
-        productRepository.saveAll(products);
-
-        return errors;
+        return createProductProcess(productsInfo);
     }
 
     // 상품 정보 수정
     @Transactional
     public void updateProduct(String productId, ProductDto.productUpdate updateDto) {
-
         ProductEntity product = productRepository.findById(Long.parseLong(productId))
                 .orElseThrow(() -> new CustomException(ErrorCode.ERROR_404, "업데이트에 실패하였습니다."));
-        product.productUpdate(updateDto);
 
+        validateProductName(productRepository, updateDto.getProductName());
+
+        product.productUpdate(updateDto);
     }
 
     //상품 조회
-//    public
+//    @Transactional
+//    public void searchProduct(String )
 
     //상품 삭제
     @Transactional
     public void deletedProduct(String productId) {
-        Optional<ProductEntity> product = productRepository.findById(Long.valueOf(productId));
-        if (product.isPresent()) {
-            productRepository.delete(product.get());
-        } else {
-            throw new CustomException(ErrorCode.ERROR_404, "유저 삭제를 실패하였습니다");
-        }
+        ProductEntity product = productRepository.findById(Long.valueOf(productId))
+                .orElseThrow(() -> new CustomException(ErrorCode.ERROR_404, "유저 삭제를 실패하였습니다"));
+        productRepository.delete(product);
+
+    }
+
+    private List<String> createProductProcess(ProductDto.productsInfo productsInfo) {
+        List<ProductEntity> products = new ArrayList<>();
+        List<String> errors = new ArrayList<>();
+
+        extractedProductInfo(productsInfo, products, errors);
+
+        productRepository.saveAll(products);
+        return errors;
     }
 
     //데이터 엔티티 리스트로 추출
-    private void extractedProductInfo(ProductDto.productsInfo productsInfo, List<ProductEntity> products, List<String> ErrorProduct) {
+    private void extractedProductInfo(ProductDto.productsInfo productsInfo, List<ProductEntity> products, List<String> errorProduct) {
+        Set<String> errorSet = new HashSet<>();
         for (ProductDto.createProduct productDto : productsInfo.products) {
-            if (validateUnionProductName(ErrorProduct, productDto)) {
-                products.add(productDto.toEntity());
+            if (validateUnionProductName(errorSet, productDto)) {
+                validateFactoryAndSave(products, productDto);
             }
         }
+        errorProduct.addAll(errorSet);
     }
 
     //예외를 발생시 해당 데이터들을 모아서 반환
-    private boolean validateUnionProductName(List<String> errors, ProductDto.createProduct productDto) {
-        if (productRepository.existsByProductNameAndProductBarcodeNumber(productDto.getModelName(), productDto.getModelBarcode())) {
-            errors.add(productDto.getModelName());
-            return false;
-        }
-        return true;
+    private boolean validateUnionProductName(Set<String> errors, ProductDto.createProduct productDto) {
+        validateExistsProductNameOrProductBarcodeNumber(productRepository, productDto.getModelName(), productDto.getModelBarcode(), errors);
+        validateExistsFactoryName(factoryRepository, productDto.getFactory(), errors);
+        return !errors.isEmpty();
     }
 
-
+    //공장 존재 확인 후 저장
+    private void validateFactoryAndSave(List<ProductEntity> products, ProductDto.createProduct productDto) {
+        FactoryEntity factory = factoryRepository.findByFactoryName(productDto.getFactory());
+        if (factory != null) {
+            products.add(productDto.toEntity(factory));
+        }
+    }
 }
