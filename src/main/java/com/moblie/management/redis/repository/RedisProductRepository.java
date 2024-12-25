4382eca042d05e4cfc509b049e05ef6fa3221b18
JS <zks145@naver.com>
2024-12-25 18:31:09 +0900

    delete: 사용하지 않는 파일 삭제

--- src/main/java/com/moblie/management/redis/repository/RedisProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.moblie.management.redis.repository;
-
-import com.moblie.management.redis.domain.ProductToken;
-import org.springframework.data.repository.CrudRepository;
-
-public interface RedisProductRepository extends CrudRepository<ProductToken, String> {
-}
