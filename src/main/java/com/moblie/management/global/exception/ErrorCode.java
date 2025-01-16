7bfeb01c9216c9d75794ab1cabc8928561fcabce
JS <zks145@naver.com>
2025-01-16 17:44:23 +0900

    miss: 누락된 커밋 "enum 변환 변수 값 변경" 포함

--- src/main/java/com/moblie/management/global/exception/ErrorCode.java
+++ src/main/java/com/moblie/management/global/exception/ErrorCode.java
@@ -12,7 +12,8 @@ public enum ErrorCode {
     ERROR_404(HttpStatus.NOT_FOUND, "리소스를 찾을 수 없습니다."),
     ERROR_403(HttpStatus.FORBIDDEN, "사용자 권한이 없습니다."),
     ERROR_401(HttpStatus.UNAUTHORIZED, "인증이 필요합니다."),
-    ERROR_400(HttpStatus.BAD_REQUEST, "잘못된 요청입니다.");
+    ERROR_400(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
+    ERROR_500(HttpStatus.INTERNAL_SERVER_ERROR, "서버 에러 발생했습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
