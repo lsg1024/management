506d15817936afb0a52039ba8f4440662b670630
JS <zks145@naver.com>
2025-04-10 17:30:38 +0900

    refactor: 토큰 만료 응답 403 -> 407로 변경

--- src/main/java/com/moblie/management/local/member/service/MemberService.java
+++ src/main/java/com/moblie/management/local/member/service/MemberService.java
@@ -173,7 +173,7 @@ public class MemberService {
         try {
             jwtUtil.isExpired(refresh);
         } catch (ExpiredJwtException e) {
-            throw new CustomException(ErrorCode.ERROR_401, "Refresh 토큰 만료");
+            throw new CustomException(ErrorCode.ERROR_407, "토큰 만료");
         }
 
         String category = jwtUtil.getCategory(refresh);
