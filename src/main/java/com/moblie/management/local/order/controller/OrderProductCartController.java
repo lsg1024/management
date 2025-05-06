8ea48cf4852113dec976f83009ab4a26c8711a32
JS <zks145@naver.com>
2025-05-06 09:16:40 +0900

    fix: 페이징 값 미설정 수정

--- src/main/java/com/moblie/management/local/order/controller/OrderProductCartController.java
+++ src/main/java/com/moblie/management/local/order/controller/OrderProductCartController.java
@@ -14,7 +14,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -96,12 +95,12 @@ public class OrderProductCartController {
     @GetMapping("/carts")
     public PageCustom<CartDto.carts> cartPage(
             @AuthenticationPrincipal PrincipalDetails principalDetails,
-            @PageableDefault() Pageable pageable) {
+            @PageableDefault(size = 20) Pageable pageable) {
 
         return orderProductCartService.getCarts(principalDetails.getId(), pageable);
     }
 
-    //장바구니 내 상품 리스트
+    //장바구니 내 상품 리스트 -> 페이징 말고 전체 값
     @GetMapping("/cart/info")
     public PageCustom<CartDto.productDetail> cartProductDetailPage(
             @AuthenticationPrincipal PrincipalDetails principalDetails,
